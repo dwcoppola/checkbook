374807e1d4dac4fa545b3fd04a0f5ece101d62f1
David Coppola <dcoppola@corp.robertcorio.com>
2021-02-10 10:23:37 -0500

    changed some urls

--- checkbook/urls.py
+++ checkbook/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:account_name_id>/', views.detail, name="account_id"),
+    path('account-<int:account_name_id>/', views.detail, name="account_detail"),
     path('create_account/', views.create_account, name="create_account"),
     path('account_confirmation/', views.account_confirmation, name="account_confirmation"),
-    path('<int:account_name_id>/create_transaction/', views.create_transaction, name="create_transaction"),
-    path('<int:account_name_id>/create_transaction/transaction_confirmation/', views.transaction_confirmation, name="transaction_confirmation"),
+    path('account-<int:account_name_id>/add_transaction/', views.create_transaction, name="add_transaction"),
+    path('account-<int:account_name_id>/add_transaction/transaction_confirmation/', views.transaction_confirmation, name="transaction_confirmation"),
 ]
